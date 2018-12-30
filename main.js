ec96924b61a08a81583d72ee8bd39781f82cce12
Bradley Ward <bradleyward@Bradleys-MBP.local.tld>
2018-12-29 20:54:32 -0500

    fixed forms not accepting decimal points

--- main.js
+++ main.js
@@ -4,15 +4,23 @@ let totalBill = 0;
 function calculateTip(event) {
   event.preventDefault();
   
-let inputOne = document.getElementById('amount').value;
-let inputTwo = document.getElementById('service').value;
+  let inputOne = document.getElementById('amount').value;
+  let inputTwo = document.getElementById('service').value;
 
-inputOne = Number(inputOne);
-inputTwo = Number(inputTwo);
+  inputOne = Number(inputOne);
+  inputTwo = Number(inputTwo);
 
   inputTwo /= 100;
   tipAmount = inputOne * inputTwo;
   totalBill = inputOne + tipAmount;
+
+  function decimalAdjustment(x) {
+    return Number.parseFloat(x).toFixed(2);
+  }
+
+  tipAmount = decimalAdjustment(tipAmount);
+  totalBill = decimalAdjustment(totalBill);
+
 
   let position = document.getElementsByTagName('p');
 
